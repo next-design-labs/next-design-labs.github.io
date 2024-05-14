3906f684c9999a938a13b8c78f6fe3fd218d6344
francisco-guilherme <francisco-guilherme@users.noreply.github.com>
2024-05-14 17:14:13 +0000

    deploy: next-design-labs/next-ui@277c2cb4adf9484b32089007d98f4e0bd2f5bd8a

---
+++ docs/_next/static/chunks/pages/_meta-2f3dfe770a66063e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[926],{2e3:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_meta",function(){return _(8676)}])},8676:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return t}});var t=!0;u.default=()=>null}},function(n){n.O(0,[888,774,179],function(){return n(n.s=2e3)}),_N_E=n.O()}]);
