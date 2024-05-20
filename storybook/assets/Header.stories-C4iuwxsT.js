9e5ce6680ce83a4b436763e850287b0f5331639d
francisco-guilherme <francisco-guilherme@users.noreply.github.com>
2024-05-20 18:19:15 +0000

    deploy: next-design-labs/next-ui@4719d080cd1e162995a2f5fce2859ea954ae84db

---
+++ storybook/assets/Header.stories-C4iuwxsT.js
@@ -0,0 +1,3 @@
+import{H as s,j as m}from"./SelectOption-fj5-K8gK.js";import"./index-CDs2tPxN.js";import"./extends-CCbyfPlC.js";import"./index-B-yFm4aN.js";const i={title:"Widgets/Header",component:s},e={render:o=>m.jsx(s,{...o})};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: (args: typeof meta) => <Header {...args} />
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,i as default};
