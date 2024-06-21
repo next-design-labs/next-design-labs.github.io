db9c067ad81b2b7911d63fa849ba3ae504441a3f
francisco-guilherme <francisco-guilherme@users.noreply.github.com>
2024-06-21 22:11:29 +0000

    deploy: next-design-labs/next-ui@1d64c24ff07e32d22d26c4bfda39632232898132

---
+++ storybook/assets/Sample.stories-CRaturkt.js
@@ -0,0 +1,3 @@
+import{j as o}from"./jsx-runtime-soFxqBSz.js";import"./index-CZlPm10g.js";const m=r=>o.jsx("div",{...r,children:"Sample"}),c={title:"Sample",component:m},e={render:r=>o.jsx(m,{...r})};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: (args: typeof meta) => <Sample {...args} />
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,c as default};
