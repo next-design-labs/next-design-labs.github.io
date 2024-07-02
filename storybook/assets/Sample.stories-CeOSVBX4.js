a9f95e0f405a440322c899b96bdf9a514a42aae9
francisco-guilherme <francisco-guilherme@users.noreply.github.com>
2024-07-02 14:46:08 +0000

    deploy: next-design-labs/next-ui@daea7de80b68f7c5b4fc4a5e6f7d98414770c29d

---
+++ storybook/assets/Sample.stories-CeOSVBX4.js
@@ -0,0 +1,3 @@
+import{j as m,t as n}from"./shadcn-45nzGUP4.js";import"./index-CZlPm10g.js";const p=r=>{var t;return m.jsx("div",{className:"text-2xl",style:{color:(t=n.text)==null?void 0:t.primary},...r,children:"Sample"})},d={title:"Sample",component:p},e={render:r=>m.jsx(p,{...r})};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: typeof meta) => <Sample {...args} />
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,d as default};
