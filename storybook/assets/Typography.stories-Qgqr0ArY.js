5d7ecd9247704468ce36e2ad2de793af8280f038
francisco-guilherme <francisco-guilherme@users.noreply.github.com>
2024-09-05 00:54:44 +0000

    deploy: next-design-labs/next-ui@dec921647d930e99030cf9d695ef12a130c9c6ef

---
+++ storybook/assets/Typography.stories-Qgqr0ArY.js
@@ -0,0 +1,14 @@
+import{N as i,j as a}from"./cva-wHoXuKHV.js";import"./index-B52nOzfP.js";const l={title:"Components/Typography",component:i,decorators:[e=>a.jsx("div",{className:"flex-col items-center gap-4",children:a.jsx(e,{})})]},t={render:()=>a.jsxs(a.Fragment,{children:[a.jsx(i,{variant:"display",children:"h1. Display"}),a.jsx(i,{variant:"title1",children:"h1. Title 1"}),a.jsx(i,{variant:"title2",children:"h2. Title 2"}),a.jsx(i,{variant:"title3",children:"h3. Title 3"}),a.jsx(i,{variant:"title4",children:"h4. Title 4"}),a.jsx(i,{variant:"body",children:"Body Text"}),a.jsx(i,{variant:"label",children:"Label Text"}),a.jsx(i,{variant:"captionLabel",children:"Caption Label"}),a.jsx(i,{variant:"caption1",children:"Caption 1"}),a.jsx(i,{variant:"caption2",children:"Caption 2"})]})};var r,n,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <>
+      <NuiTypography variant="display">h1. Display</NuiTypography>
+      <NuiTypography variant="title1">h1. Title 1</NuiTypography>
+      <NuiTypography variant="title2">h2. Title 2</NuiTypography>
+      <NuiTypography variant="title3">h3. Title 3</NuiTypography>
+      <NuiTypography variant="title4">h4. Title 4</NuiTypography>
+      <NuiTypography variant="body">Body Text</NuiTypography>
+      <NuiTypography variant="label">Label Text</NuiTypography>
+      <NuiTypography variant="captionLabel">Caption Label</NuiTypography>
+      <NuiTypography variant="caption1">Caption 1</NuiTypography>
+      <NuiTypography variant="caption2">Caption 2</NuiTypography>
+    </>
+}`,...(p=(n=t.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const h=["Main"];export{t as Main,h as __namedExportsOrder,l as default};
